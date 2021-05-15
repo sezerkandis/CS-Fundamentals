0d48855a9ef20f31b010b69338aa0e9857bbe149
sezerkandis <32538370+sezerkandis@users.noreply.github.com>
2021-05-16 00:38:14 +0300

    4 solutions added

---
+++ interview_preparation_kit/1_warm_up_challanges/4_repeated_string.cpp
@@ -0,0 +1,82 @@
+// https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+long repeatedString(string s, long n) {
+    // Enter your code here.
+    // --------------------------------------------------------- //  
+    int length = s.length();
+    long count = 0;
+    for (int i = 0; i < length; i++) {
+        if (s[i] == 'a') {
+            count++;
+        }
+    }
+    count = count * (n / length);
+    int left_count = n % length;
+    for (int i = 0; i < left_count; i++) {
+        if (s[i] == 'a') {
+            count++;
+        }
+    }
+    
+    return count;
+    // --------------------------------------------------------- // 
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    long n = stol(ltrim(rtrim(n_temp)));
+
+    long result = repeatedString(s, n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
