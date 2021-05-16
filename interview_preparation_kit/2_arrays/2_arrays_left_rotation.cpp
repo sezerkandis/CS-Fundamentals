52dfa3b98ae1f5b4007ae957419244f6c2133a51
sezerkandis <32538370+sezerkandis@users.noreply.github.com>
2021-05-16 20:22:10 +0300

    added question link

--- interview_preparation_kit/2_arrays/2_arrays_left_rotation.cpp
+++ interview_preparation_kit/2_arrays/2_arrays_left_rotation.cpp
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays&h_r=next-challenge&h_v=zen
+
 #include <bits/stdc++.h>
 
 using namespace std;
