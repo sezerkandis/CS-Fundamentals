dd8876639aaf8ae42c85b4d7d8cae8a94f43c934
sezerkandis <32538370+sezerkandis@users.noreply.github.com>
2021-11-04 21:46:16 +0300

    solutions added

---
+++ cpp/35_attending _workshops.cpp
@@ -0,0 +1,86 @@
+// https://www.hackerrank.com/challenges/attending-workshops/problem?isFullScreen=true
+
+#include<bits/stdc++.h>
+#include<bits/stdc++.h>
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#include "iostream"
+#include <vector>
+
+//Define the structs Workshops and Available_Workshops.
+//Implement the functions initialize and CalculateMaxWorkshops
+
+// Enter your code here.
+// --------------------------------------------------------- // 
+struct Workshop
+{
+    int start_time;
+    int duration;
+    int end_time;
+};
+
+struct Available_Workshops
+{
+    int n = 0;
+    vector<Workshop> workshops;
+};
+
+Available_Workshops* initialize(int start_time[], int duration[], int n)
+{
+    Available_Workshops* ptr = new Available_Workshops;    
+    if (n >= 1 && n <= 100000)
+    {
+        ptr->n = n;
+        for (int i = 0; i < n; i++) {
+            Workshop ws;
+            ws.start_time = start_time[i];
+            ws.duration = duration[i];
+            ws.end_time = ws.start_time + ws.duration;
+            ptr->workshops.push_back(ws);
+        }
+    }
+    return ptr;
+}
+
+int CalculateMaxWorkshops(Available_Workshops * ptr)
+{        
+    std::sort(ptr->workshops.begin(), ptr->workshops.end(), [](const Workshop  &a, const Workshop &b)
+    { 
+        return a.end_time < b.end_time; 
+    });
+    
+    int end_time = 0;
+    int count = 0;
+    
+    for (int i = 0; i < ptr->n; i++) {
+        if (ptr->workshops[i].start_time >= end_time) {
+            end_time = ptr->workshops[i].end_time;
+            count++;
+        }
+    }    
+    return count;
+}
+// --------------------------------------------------------- // 
+
+int main(int argc, char *argv[]) {
+    int n; // number of workshops
+    cin >> n;
+    // create arrays of unknown size n
+    int* start_time = new int[n];
+    int* duration = new int[n];
+
+    for(int i=0; i < n; i++){
+        cin >> start_time[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> duration[i];
+    }
+
+    Available_Workshops * ptr;
+    ptr = initialize(start_time,duration, n);
+    cout << CalculateMaxWorkshops(ptr) << endl;
+    return 0;
+}
---
+++ cpp/36_messages_order.cpp
@@ -0,0 +1,80 @@
+// https://www.hackerrank.com/challenges/messages-order/problem?isFullScreen=true
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+// Enter your code here.
+// --------------------------------------------------------- // 
+class Message {
+public: 
+    Message() {}
+    Message(const int& id, const std::string& text) : m_id{id}, m_text{text} { }
+    const string& get_text() {
+        return m_text;
+    }    
+    bool operator < (const Message& msg) {
+        return m_id < msg.m_id;
+    }
+private:
+    std::string m_text;
+    int m_id;
+};
+
+class MessageFactory {
+public:
+    MessageFactory() { m_id = 0; }
+    Message create_message(const string& text) {
+        return Message(m_id++, text);
+    }
+private:
+    int m_id;
+};
+// --------------------------------------------------------- // 
+
+class Recipient {
+public:
+    Recipient() {}
+    void receive(const Message& msg) {
+        messages_.push_back(msg);
+    }
+    void print_messages() {
+        fix_order();
+        for (auto& msg : messages_) {
+            cout << msg.get_text() << endl;
+        }
+        messages_.clear();
+    }
+private:
+    void fix_order() {
+        sort(messages_.begin(), messages_.end());
+    }
+    vector<Message> messages_;
+};
+
+class Network {
+public:
+    static void send_messages(vector<Message> messages, Recipient& recipient) {
+    // simulates the unpredictable network, where sent messages might arrive in unspecified order
+        random_shuffle(messages.begin(), messages.end());         
+        for (auto msg : messages) {
+            recipient.receive(msg);
+        }
+    }
+};
+
+
+
+int main() {
+    MessageFactory message_factory;
+    Recipient recipient;
+    vector<Message> messages;
+    string text;
+    while (getline(cin, text)) {
+        messages.push_back(message_factory.create_message(text));
+    }
+    Network::send_messages(messages, recipient);
+    recipient.print_messages();
+}
