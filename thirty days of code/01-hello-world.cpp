e01e501670322a09cffda297ff28c5e36bcd2c64
sezerkandis <32538370+sezerkandis@users.noreply.github.com>
2022-05-22 00:38:17 +0300

    Create 01-hello-world.cpp

---
+++ thirty days of code/01-hello-world.cpp
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/30-hello-world/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+   string inputString; // declare a variable to hold our input
+   getline(cin, inputString); // get a line of input from cin and save it to our variable
+  
+   // Your first line of output goes here
+   cout << "Hello, World." << endl;
+
+// Enter your code here.
+// --------------------------------------------------------- // 
+   std::cout << inputString << std::endl;
+// --------------------------------------------------------- // 
+
+   return 0;
+}
