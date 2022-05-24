dfafe25e0785aba180329faeba121e6681cbd464
sezerkandis <32538370+sezerkandis@users.noreply.github.com>
2022-05-25 00:08:32 +0300

    Create strange-advertising.cpp

---
+++ problem solving/02-implementation/strange-advertising.cpp
@@ -0,0 +1,68 @@
+// https://www.hackerrank.com/challenges/strange-advertising/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'viralAdvertising' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+// Enter your code here.
+// --------------------------------------------------------- // 
+int viralAdvertising(int n) {
+    int like_count = 0, shared = 5;
+    for (int day = 0; day < n; day++) {
+        int liked = floor(shared / 2);
+        like_count += liked;
+        shared = liked * 3;
+    }
+    return like_count;
+}
+// --------------------------------------------------------- // 
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    int result = viralAdvertising(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
