b953aff1ed40df592e031b9b4c9b81cf388f3f98
sezerkandis <32538370+sezerkandis@users.noreply.github.com>
2022-05-27 00:07:17 +0300

    Create Day 17-more-exceptions.cpp

---
+++ thirty days of code/Day 17-more-exceptions.cpp
@@ -0,0 +1,40 @@
+// https://www.hackerrank.com/challenges/30-more-exceptions/
+   
+#include <cmath>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+using namespace std;
+
+// Enter your code here.
+// --------------------------------------------------------- // 
+class Calculator
+{
+public:
+    int power(int n, int p) {
+        if (n >= 0 && p >= 0) {
+            return pow(n, p);
+        }
+        throw std::invalid_argument("n and p should be non-negative");
+    }
+};
+// --------------------------------------------------------- // 
+
+int main()
+{
+    Calculator myCalculator=Calculator();
+    int T,n,p;
+    cin>>T;
+    while(T-->0){
+      if(scanf("%d %d",&n,&p)==2){
+         try{
+               int ans=myCalculator.power(n,p);
+               cout<<ans<<endl; 
+         }
+         catch(exception& e){
+             cout<<e.what()<<endl;
+         }
+      }
+    }
+    
+}
